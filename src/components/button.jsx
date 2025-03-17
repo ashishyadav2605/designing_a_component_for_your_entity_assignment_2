dfbdbd521992b731c326be7b5495f4b02fa40454
Ashish <iashishyadav17@gmai.com>
2025-03-17 09:55:47 +0530

    updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,26 @@
-// write button card here
+// components/button.jsx
+import React from 'react';
+
+const Button = () => {
+  const buttonStyle = {
+    backgroundColor: 'crimson',
+    color: 'white',
+    padding: '8px 16px',
+    border: 'none',
+    borderRadius: '4px',
+    fontSize: '14px',
+    transition: 'background-color 0.3s',
+  };
+
+  return (
+    <button 
+      style={buttonStyle}
+      onMouseOver={(e) => e.target.style.backgroundColor = 'red'}
+      onMouseOut={(e) => e.target.style.backgroundColor = 'crimson'}
+    >
+      View Product
+    </button>
+  );
+};
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import Button from "./button";
+
+const Productcard = () => { 
+    const productName = 'Apple Air Pods Max';
+    const productImage = 'https://m.media-amazon.com/images/I/71j3WqqU7WL.AC_UF1000,1000_QL80.jpg';
+    const price = '$569.69';
+    
+    return(
+        <div>
+            <h3>Name of Product: {productName} </h3>
+            <img style={{width: "200px"}} src={productImage} alt="product image"/>
+            <p><strong>Price: {price} </strong></p>
+            <Button/>
+        </div>
+    )
+}
+
+
+export default Productcard;
